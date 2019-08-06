f9e41285c79ba343d6f45b67aa34ee34a8e8f85e
wkwk2805 <wkwk2805@gmail.com>
2019-08-06 23:09:17 +0900

    mobile project init

---
+++ backend-mobile/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, () => {
+  console.log("Connect Server 3000port");
+});
